ec2dfb11e18e2833316dbd157d1a508137043b5b
matgrand <matteo.grandin@gmail.com>
2024-12-30 12:44:34 +0100

    rfx simulation 93% OK, real training OK, understood role of "normalization"

--- common.py
+++ common.py
@@ -13,6 +13,7 @@ KHRES = 2 # [#] multiplier for the high resolution of the grid
 RES = GRID_SIZE*KHRES # [#] high resolution of the grid in pixels (square grid)
 L = 1.101600170135498 # [m] length of the grid in the r/x direction (square grid)
 R0 = 1.449199914932251 # [m] grid start in the r/x direction
+R_FW = 0.4905 # [m] first wall radius
 Z0 = -0.5508000254631042 # [m] grid start in the z/y direction
 R1, Z1 = R0+L, Z0+L # [m] grid ends in the r/x and z/y direction
 RM, ZM = 0.5*(R0+R1), 0.5*(Z0+Z1) # [m] grid center in the x/r and z direction
@@ -24,41 +25,44 @@ assert np.isclose(R1-R0, Z1-Z0), "grid must be square"
 RRH, ZZH = np.meshgrid(R, Z) # create a grid of R and Z values
 RRL, ZZL = RRH[::KHRES, ::KHRES], ZZH[::KHRES, ::KHRES] # create low resolution grid
 RZ = np.stack((RRH, ZZH), axis=-1) # create a grid of R and Z values
-FW = np.array([[RM+L/2*np.cos(θ), ZM+L/2*np.sin(θ)] for θ in np.linspace(0, 2*π, 100)]) # [m] first wall
+FW = np.array([[RM+R_FW*np.cos(θ), ZM+R_FW*np.sin(θ)] for θ in np.linspace(0, 2*π, 100)]) # [m] first wall
+
 
 # RFX SXR Parameters (all aproximated)
+
+# RFX REAL CLEAN DATASET SXR (tot 92) SUBDIVISIONS (my data interpretation)
+VDI_INTERVAL = (32, 49) 
+VDC_INTERVAL = (16, 32)
+VDE_INTERVAL = (0, 16) 
+HOR1_INTERVAL = (49, 68)
+HOR2_INTERVAL = (68, 92)
+
 # VDI, SXR Vertical Internal 
 VDI_SPAN_ANGLE = 0.32004687510760094
 VDI_START_ANGLE = π/2 - VDI_SPAN_ANGLE/2
 VDI_PINHOLE_POS = (1.72, -0.73) 
 VDI_NRAYS = 19 # number of rays (paper)
-VDI_TO_KEEP = np.arange(3, 19) # rays to keep (data) 16 (could be 2->18)
+VDI_TO_KEEP = np.arange(1, 18) # rays to keep (data) 17 (could be 0->17, 2->19) #
 
 # VDC, SXR Vertical Central
 VDC_SPAN_ANGLE = 0.7859524793194145
 VDC_START_ANGLE = π/2 - VDC_SPAN_ANGLE/2
 VDC_PINHOLE_POS = (2.0, -0.64) 
 VDC_NRAYS = 19 # number of rays (paper)
-VDC_TO_KEEP = np.arange(1, 17) # rays to keep (data) 16 (could be 2->18)
+VDC_TO_KEEP = np.arange(2, 18) # rays to keep (data) 16 (could be 2->18)
 
 # VDE, SXR Vertical External
 VDE_SPAN_ANGLE = 0.9004511925214301
 VDE_START_ANGLE = π/2 - VDE_SPAN_ANGLE/2
 VDE_PINHOLE_POS = (2.27, -0.46) 
 VDE_NRAYS = 19 # number of rays (paper)
-VDE_TO_KEEP = np.arange(2, 19) # rays to keep (data) 17 (could be 1->18)
+VDE_TO_KEEP = np.arange(1, 17) # rays to keep (data) 16 (could be 0->16, 2->18, 3->19)
 
 # HOR, SXR Horizontal, double
-HOR_SPAN_ANGLE = 1.2495650930032198
+HOR_SPAN_ANGLE = 1.5 #(1.2495650930032198 from paper)
 HOR_START_ANGLE = π - HOR_SPAN_ANGLE/2
 HOR_PINHOLE_POS = (2.53, 0.0)
-HOR_NRAYS = 21 # number of rays (paper)
-HOR1_TO_KEEP = np.arange(1, 20) # rays to keep (data) 19 (may be different)
-HOR2_TO_KEEP = np.arange(0, 24) # rays to keep (data) all of them, 24 # NOTE: impossible with 21 rays, ignore HOR2
+HOR_NRAYS = 21 # number of rays (paper 24)
 
-# RFX REAL CLEAN DATASET SXR (tot 92) SUBDIVISIONS (my data interpretation)
-VDI_INTERVAL = (0, 16)
-VDC_INTERVAL = (16, 32)
-VDE_INTERVAL = (32, 49)
-HOR1_INTERVAL = (49, 68)
-HOR2_INTERVAL = (68, 92)
+HOR1_TO_KEEP = np.arange(2, 21) # rays to keep (data) 19 (may be different)
+#HOR2_TO_KEEP = np.arange(0, 24) # rays to keep (data) all of them, 24 # NOTE: impossible with 21 rays, ignore HOR2
